6b86dc57fe71cf32993b7989a0637bc11f61e055
AdrianChingHC <achc92@gmail.com>
2015-05-02 05:32:03 +0800

    Add path for pin, index new and create action for controller, view files

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,19 @@
 class PinsController < ApplicationController
+	def index
+		
+	end
+
+	def new
+		@pin = Pins.new
+	end
+
+	def create
+		@pin = Pins.new(pin_params)
+	end
+
+	private
+
+	def pin_params
+		params.require(:pin).permit(:title, :description)
+	end
 end
